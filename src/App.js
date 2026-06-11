41affe308e874220450f19a73acecda62b639dad
c-sparkz <c.sparkz.empire@gmail.com>
2026-06-11 09:39:59 -0500

    working build with admin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import AdminDashboard from './AdminDashboard';import { useState } from "react";
+import AdminDashboard from './AdminDashboard_1';import { useState } from "react";
 
 const GOLD = "#C9A84C";
 const GOLD_LIGHT = "#E8C96A";
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import AdminDashboard from './AdminDashboard';
-
+import AdminDashboard from './AdminDashboard_1';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 if (window.location.pathname === '/admin') {
